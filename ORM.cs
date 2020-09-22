e9d15aa3f6bf5a89b5cd2643bd46766a4c646c39
rquanx <runquant@gmail.com>
2020-09-23 03:47:36 +0800

    feat: 增加IN参数支持

--- ORM.cs
+++ ORM.cs
@@ -13,19 +13,19 @@ namespace ORM
 {
     public static class ORM
     {
-        private static object GetClassProperty(IDataReader reader,Type type)
+        private static object GetClassProperty(IDataReader reader, Type type)
         {
             var instance = Activator.CreateInstance(type);
             var fieldDic = new Dictionary<string, string>();
-            for(var i = 0;i < reader.FieldCount; i++)
+            for (var i = 0; i < reader.FieldCount; i++)
             {
                 var field = reader.GetName(i);
                 fieldDic.Add(field, field);
             }
-            
-            foreach(var prop in type.GetProperties())
+
+            foreach (var prop in type.GetProperties())
             {
-                var field = ORMAttributeHelper.GetPropField(prop); 
+                var field = ORMAttributeHelper.GetPropField(prop);
                 if (!fieldDic.ContainsKey(field)) continue;
                 if (prop.PropertyType.IsPrimitive || prop.PropertyType.Equals(typeof(string)))
                 {
@@ -41,7 +41,7 @@ namespace ORM
 
         private static dynamic GetDynamicProperty(IDataReader reader)
         {
-            var instance = new ExpandoObject(); 
+            var instance = new ExpandoObject();
             var dic = (IDictionary<string, object>)instance;
             for (var i = 0; i < reader.FieldCount; i++)
             {
@@ -51,26 +51,49 @@ namespace ORM
             return instance;
         }
 
-        private static void AddParameter(IDbCommand cmd,object item)
+        private static void AddParameter(IDbCommand cmd, object item)
         {
             var type = item.GetType();
-            foreach(var prop in type.GetProperties())
+            foreach (var prop in type.GetProperties())
             {
-                var param = new SqlParameter($"@{prop.Name}",prop.GetValue(item));
-                cmd.Parameters.Add(param);
+                var v = prop.GetValue(item);
+                if (v is ICollection)
+                {
+                    HandleInClause(cmd, prop.Name, v as ICollection);
+                }
+                else
+                {
+                    var param = new SqlParameter($"@{prop.Name}", prop.GetValue(item));
+                    cmd.Parameters.Add(param);
+                }
             }
         }
 
-        private static IDbCommand CreateCommand(IDbConnection cnn, string sql,object param)
+        private static void HandleInClause(IDbCommand cmd, string field, ICollection obj)
+        {
+            var replces = new List<string>();
+            var count = 0;
+            foreach (var item in obj)
+            {
+                var newField = $@"@{field}{count}";
+                replces.Add(newField);
+                var param = new SqlParameter(newField, item);
+                cmd.Parameters.Add(param);
+                count++;
+            }
+            cmd.CommandText = cmd.CommandText.Replace($@"@{field}", $@"({string.Join(",", replces)})");
+        }
+
+        private static IDbCommand CreateCommand(IDbConnection cnn, string sql, object param)
         {
             var cmd = cnn.CreateCommand();
             cmd.CommandText = sql;
-            
+
             if (param is IEnumerable o)
             {
                 foreach (var item in o)
                 {
-                    AddParameter(cmd,item);
+                    AddParameter(cmd, item);
                 }
             }
             else
@@ -83,7 +106,7 @@ namespace ORM
         public static List<T> Query<T>(this IDbConnection cnn, string sql, object param = null)
         {
             var dataList = new List<T>();
-            var cmd = CreateCommand(cnn,sql, param);
+            var cmd = CreateCommand(cnn, sql, param);
             var type = typeof(T);
             var reader = cmd.ExecuteReader();
             while (reader.Read())
